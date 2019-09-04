4044ffc39f2bea38ee1539165e78c54862d879f3
accgit <zdenek.papucik@gmail.com>
2019-09-04 12:31:34 +0200

    dev

---
+++ dist/js/sidebar.menu.js
@@ -0,0 +1,68 @@
+/*!
+ * Sidebar menu for Bootstrap 4
+ * Copyright Zdeněk Papučík
+ * MIT License
+ */
+(function($) {
+
+  // class with arrow icon
+  var linkArrow     = '.link-arrow';
+
+  // class current link
+  var linkCurrent   = '.link-current';
+
+  // class hidden list
+  var listHidden    = '.list-hidden';
+
+  // class list item
+  var listItem      = '.list-item';
+
+  // toggle sidebar menu
+  $('#sidebar-toggle').on('click', function() {
+    $('#wrapper').toggleClass('sidebar-toggle');
+  });
+
+  // list init
+  $(listItem).each(function() {
+
+    var el = $(this);
+    var parent  = el.parent();
+
+    // current link class
+    var current = parent.find(linkArrow + linkCurrent);
+
+    // default icon menu
+    parent.find(linkArrow).addClass('up');
+
+    // view the list above the current link
+    if (current.length > 0) {
+
+      // active down icon
+      current.addClass('active down');
+
+      // show hidden list
+      current.next(listHidden).show();
+    }
+  });
+
+  // list transition arrow
+  $(linkArrow).on('click', function() {
+
+    var el = $(this);
+
+    // adding rotation effect to arrows icons
+    el.addClass('transition').toggleClass('active rotate');
+
+    // adding link current on click link
+    !(el.hasClass('link-current')) ? el.addClass('link-current') : el.removeClass('link-current');
+
+    // show hidden list
+    el.next(listHidden).slideToggle('fast');
+
+    // rotate the direction of rotation of the arrow
+    if (el.parent().find(linkArrow).hasClass('down')) {
+      el.toggleClass('rotate-revert');
+    }
+  });
+
+}(jQuery));
---
+++ readme.md
@@ -0,0 +1,50 @@
+## Sidebar menu
+Simple and fast multi-level sidebar menu 2 on Bootstrap 4
+
+## Installation
+```
+npm install --save sidebar-menu-compostrap
+```
+
+## Built on
+
+- Bootstrap 4
+- jQuery 3
+- Font Awesome 5
+- Perfect scrollbar
+- Open Sans @font-face kit
+- Nanobar
+
+## Live preview
+
+- https://compostrap.github.io/sidebar.menu
+
+## Themes
+The Sidebar is basically a light and dark theme.
+
+White style
+```html
+<nav role="navigation" class="sidebar-white">
+```
+
+Dark style
+```html
+<nav role="navigation" class="sidebar-dark">
+```
+
+It is also possible to color the menu
+```html
+<nav role="navigation" class="sidebar-menu-magenta">
+<nav role="navigation" class="sidebar-menu-purple">
+<nav role="navigation" class="sidebar-menu-orange">
+<nav role="navigation" class="sidebar-menu-cyan">
+<nav role="navigation" class="sidebar-menu-green">
+```
+
+If you want to add more topics or customize the theme colors, edit the following sass files.
+```
+scss/themes/_colors.scss
+scss/themes/_theme.scss
+```
+
+Enjoy the menu and give it a star if you like it.
